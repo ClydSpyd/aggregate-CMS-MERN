776c26a6777d84a9b397251c7b72fe7d0ff57c4f
Dave Clydesdale <47127754+ClydSpyd@users.noreply.github.com>
2024-12-02 18:58:34 +0100

    upload-routes-deactivated

--- server.js
+++ server.js
@@ -13,12 +13,14 @@ const PORT = process.env.PORT || 6969;
 app.use(cors());
 app.use(express.json());
 
+console.log("starting server...");
+
 connectDB();
 
 app.use("/api/test", testRoutes);
 app.use("/api/rss", rssRoutes);
 app.use("/api/article", articleRoutes);
-app.use("/api/upload", uploadRoutes);
+// app.use("/api/upload", uploadRoutes);
 
 // Serve static files from the React app's build folder
 app.use(express.static(path.join(__dirname, 'client/build')));
